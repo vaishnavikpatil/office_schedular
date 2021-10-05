e9e0d4731da90b4c068e11eeff57667d1e245d9d
akshata <akshataatpadkar511@gmail.com>
2021-10-05 20:12:36 +0530

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 //@dart=2.9
-import 'package:flutter/material.dart'  show Colors, MaterialApp, ThemeData, WidgetsFlutterBinding, runApp;
+import 'package:flutter/material.dart'
+    show Colors, MaterialApp, ThemeData, WidgetsFlutterBinding, runApp;
+import 'package:firebase_core/firebase_core.dart';
 import './app_screens/login_screen.dart';
 import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
+
 void main()
 async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
